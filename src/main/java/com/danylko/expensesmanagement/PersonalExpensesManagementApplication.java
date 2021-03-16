1bd7ce525c2b54a5fb0d60ebc845a4f86dc6ab2a
DanylkoMykola <danylkomykola@gmail.com>
2021-03-16 20:40:36 +0300

    First commit

---
+++ src/main/java/com/danylko/expensesmanagement/PersonalExpensesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.danylko.expensesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpensesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalExpensesManagementApplication.class, args);
+    }
+
+}
